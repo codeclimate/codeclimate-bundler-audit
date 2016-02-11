77e07fc513d6fc1e74e93a850a8fab0ce3cdc676
Devon Blandin <dblandin@gmail.com>
2016-02-11 16:54:02 -0500

    Cleanup instance variables

--- lib/cc/engine/bundler_audit/analyzer.rb
+++ lib/cc/engine/bundler_audit/analyzer.rb
@@ -4,20 +4,20 @@ module CC
       class Analyzer
         GemfileLockNotFound = Class.new(StandardError)
 
-        def initialize(directory: , io: , engine_config: )
+        def initialize(directory:, io: STDOUT, engine_config: {})
           @directory = directory
-          @engine_config = engine_config
           @io = io
+          @engine_config = engine_config
         end
 
         def run
           if gemfile_lock_exists?
-            Dir.chdir(@directory) do
+            Dir.chdir(directory) do
               Bundler::Audit::Scanner.new.scan do |vulnerability|
                 result = Result.new(vulnerability, File.open(gemfile_lock_path))
                 issue = result.to_issue
 
-                @io.print("#{issue.to_json}\0")
+                io.print("#{issue.to_json}\0")
               end
             end
           else
@@ -27,12 +27,14 @@ module CC
 
         private
 
+        attr_reader :directory, :io
+
         def gemfile_lock_exists?
           File.exist?(gemfile_lock_path)
         end
 
         def gemfile_lock_path
-          File.join(@directory, "Gemfile.lock")
+          File.join(directory, "Gemfile.lock")
         end
       end
     end
--- spec/cc/engine/bundler_audit/analyzer_spec.rb
+++ spec/cc/engine/bundler_audit/analyzer_spec.rb
@@ -4,18 +4,18 @@ module CC::Engine::BundlerAudit
   describe Analyzer do
     describe "#run" do
       it "raises an error when no Gemfile.lock exists" do
-        directory = File.join(Dir.pwd, "spec", "fixtures", "no_gemfile_lock")
+        directory = fixture_directory("no_gemfile_lock")
         io = StringIO.new
 
-        expect { Analyzer.new(directory: directory, io: io, engine_config: {}).run }
+        expect { Analyzer.new(directory: directory, io: io).run }
           .to raise_error(Analyzer::GemfileLockNotFound)
       end
 
       it "emits issues for Gemfile.lock problems" do
         io = StringIO.new
-        directory = File.join(Dir.pwd, "spec", "fixtures", "unpatched_versions")
+        directory = fixture_directory("unpatched_versions")
 
-        audit = Analyzer.new(directory: directory, io: io, engine_config: {})
+        audit = Analyzer.new(directory: directory, io: io)
         audit.run
 
         issues = io.string.split("\0").map { |issue| JSON.load(issue) }
@@ -24,9 +24,13 @@ module CC::Engine::BundlerAudit
       end
 
       def expected_issues(fixture)
-        path = File.join(Dir.pwd, "spec", "fixtures", fixture, "issues.json")
+        path = File.join(fixture_directory(fixture), "issues.json")
         body = File.read(path)
         JSON.load(body)
+      end
+
+      def fixture_directory(fixture)
+        File.join(Dir.pwd, "spec", "fixtures", fixture)
       end
     end
   end
