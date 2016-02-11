03f863d4a7c44b948d5ab0bce2fdf4594e80f1d6
Devon Blandin <dblandin@gmail.com>
2016-02-11 16:49:11 -0500

    Move remediation points to constants

--- lib/cc/engine/bundler_audit/remediation.rb
+++ lib/cc/engine/bundler_audit/remediation.rb
@@ -2,6 +2,12 @@ module CC
   module Engine
     module BundlerAudit
       class Remediation
+        MAJOR_UPGRADE_POINTS = 50_000_000
+        MINOR_UPGRADE_POINTS = 5_000_000
+        PATCH_UPGRADE_POINTS = 500_000
+        UNPATCHED_VERSION_POINTS = 500_000_000
+
+
         def initialize(gem_version, patched_versions)
           @gem_version = gem_version
           @patched_versions = patched_versions
@@ -12,15 +18,15 @@ module CC
             upgrade_versions.map do |upgrade_version|
               case
               when current_version.major != upgrade_version.major
-                50_000_000
+                MAJOR_UPGRADE_POINTS
               when current_version.minor != upgrade_version.minor
-                5_000_000
+                MINOR_UPGRADE_POINTS
               when current_version.tiny != upgrade_version.tiny
-                500_000
+                PATCH_UPGRADE_POINTS
               end
             end.min
           else
-            500_000_000
+            UNPATCHED_VERSION_POINTS
           end
         end
 
